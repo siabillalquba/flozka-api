7fc2e2a1ce7ede7eedd4d7343b128aa9465b72f2
Sia Billal Quba <siabillal@gmail.com>
2025-08-02 19:37:31 +0700

    feat: prisma client and get products

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { Hono } from "hono";
+import { prisma } from "./lib/prisma";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.json({
     message: "Flozka API",
+    products: "/products",
   });
+});
+
+app.get("/products", async (c) => {
+  const products = await prisma.product.findMany();
+
+  return c.json(products);
 });
 
 export default app;
