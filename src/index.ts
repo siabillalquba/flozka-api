e63436a90681c32e2bfc4bce2a03ee6fb0a1411f
Sia Billal Quba <siabillal@gmail.com>
2025-08-08 19:45:23 +0700

    feat: setup openapi and scalar

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,39 @@
-import { Hono } from "hono";
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 import { prisma } from "./lib/prisma";
 import { cors } from "hono/cors";
+import { ProductsSchema } from "./modules/product/schema";
+import { Scalar } from "@scalar/hono-api-reference";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
 app.use(cors());
 
-app.get("/", (c) => {
-  return c.json({
-    message: "Flozka API",
-    products: "/products",
-  });
+app.openapi(
+  createRoute({
+    method: "get",
+    path: "/products",
+    responses: {
+      200: {
+        content: { "application/json": { schema: ProductsSchema } },
+        description: "Get all products",
+      },
+    },
+  }),
+  async (c) => {
+    const products = await prisma.product.findMany();
+
+    return c.json(products);
+  }
+);
+
+app.doc("/openapi.json", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Flozka API",
+  },
 });
 
-app.get("/products", async (c) => {
-  const products = await prisma.product.findMany();
-
-  return c.json(products);
-});
+app.get("/", Scalar({ url: "/openapi.json" }));
 
 export default app;
