9da2987551cff168e6ab313dc23a3c3d5e8e1cfe
Sia Billal Quba <siabillal@gmail.com>
2025-08-22 00:12:15 +0700

    feat: user login route

--- src/modules/auth/route.ts
+++ src/modules/auth/route.ts
@@ -1,7 +1,7 @@
 import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 import { prisma } from "../../lib/prisma";
 import { PrivateUserSchema } from "../user/schema";
-import { AuthRegisterSchema } from "./schema";
+import { AuthLoginSchema, AuthRegisterSchema } from "./schema";
 
 export const authRoute = new OpenAPIHono();
 
@@ -32,6 +32,43 @@ authRoute.openapi(
         fullName: body.fullName,
       },
     });
+
+    return c.json(user);
+  }
+);
+
+authRoute.openapi(
+  createRoute({
+    method: "post",
+    path: "/login",
+    request: {
+      body: {
+        content: {
+          "application/json": { schema: AuthLoginSchema },
+        },
+      },
+    },
+    responses: {
+      200: {
+        content: { "application/json": { schema: PrivateUserSchema } },
+        description: "Login success",
+      },
+      404: {
+        description: "User not found",
+      },
+    },
+  }),
+  async (c) => {
+    const body = c.req.valid("json");
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+    if (!user) {
+      return c.notFound();
+    }
 
     return c.json(user);
   }
