bd52f50a1a1f423b39e9c0944f65fe02fcf0c1a2
Sia Billal Quba <siabillal@gmail.com>
2025-09-21 23:30:25 +0700

    feat: add item to cart endpoint

--- src/modules/cart/route.ts
+++ src/modules/cart/route.ts
@@ -4,7 +4,7 @@ import { PrivateUserSchema } from "../user/schema";
 import { AuthHeaderSchema } from "../auth/schema";
 
 import { checkAuthorized } from "../auth/middleware";
-import { CartSchema } from "./schema";
+import { AddCartItemSchema, CartItemSchema, CartSchema } from "./schema";
 
 export const cartRoute = new OpenAPIHono();
 
@@ -45,5 +45,57 @@ cartRoute.openapi(
     }
 
     return c.json(cart);
+  }
+);
+
+// POST /cart/items
+cartRoute.openapi(
+  createRoute({
+    method: "post",
+    path: "/items",
+    request: {
+      body: { content: { "application/json": { schema: AddCartItemSchema } } },
+    },
+    middleware: checkAuthorized,
+    responses: {
+      200: {
+        content: { "application/json": { schema: CartItemSchema } },
+        description: "Add item to cart",
+      },
+      400: {
+        description: "Failed to add item to cart",
+      },
+      404: {
+        description: "Cart not found",
+      },
+    },
+  }),
+  async (c) => {
+    try {
+      const body = c.req.valid("json");
+      const user = c.get("user");
+
+      const cart = await prisma.cart.findFirst({
+        where: { userId: user.id },
+        include: { items: { include: { product: true } } },
+      });
+
+      if (!cart) {
+        return c.json({ message: "Cart not found" }, 404);
+      }
+
+      const newCartItem = await prisma.cartItem.create({
+        data: {
+          cartId: cart.id,
+          productId: body.productId,
+          quantity: body.quantity,
+        },
+        include: { product: true },
+      });
+
+      return c.json(newCartItem);
+    } catch (error) {
+      return c.json({ message: "Failed to add item to cart" }, 400);
+    }
   }
 );
